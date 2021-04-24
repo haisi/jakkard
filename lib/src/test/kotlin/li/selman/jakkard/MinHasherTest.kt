3de89b4d7a577ebc861702c90d994795d3fab926
Hasan Kara <hasan.selman.kara@gmail.com>
2021-04-25 00:22:46 +0200

    Update MinHasher test docs

--- lib/src/test/kotlin/li/selman/jakkard/MinHasherTest.kt
+++ lib/src/test/kotlin/li/selman/jakkard/MinHasherTest.kt
@@ -3,20 +3,32 @@ package li.selman.jakkard
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.io.*
+import kotlin.math.abs
 
 /**
  * Testing MinHashes is rather difficult as we "randomly" generating many hash-functions, hashing string-tokens and
  * then picking the lowest hash-value — which itself is not really determinable by humans.
- * However, we can logic and heuristic based tests.
+ *
+ * However, we can use logic and heuristic based tests.
  * For the logic based test we test certain properties we know must be right.
  * For the heuristic based test, we use text-sources (for example Wikipedia articles), mutate them all slightly and
  * compare them.
+ *
  * In a sense this is a plagiarism test, where we expect the slightly mutated texts to be similar as they originate
  * from the same source.
+ *
+ * Furthermore, we can compare the similarity of the MinHash version with the Shingles version.
+ * The Shingles can be considered as the baseline for the accuracy.
+ * Whereas the MinHash is used to reduce time and space complexity compare to Shingles — especially in combinations
+ * with locality sensitive hashing.
+ *
+ * Also, we can tweak the parameters. For example increasing the number of hash-functions should increase the accuracy.
  */
 internal class MinHasherTest {
 
     // TODO(#test): Create a test which calculates the MinHash for the same file and calcs the similarity → 1.0
+
+    // TODO(#test): Create a test where we increase the number of hashes and see if delta to shingles get smaller
 
     @Test
     fun tokenize() {
@@ -31,9 +43,15 @@ internal class MinHasherTest {
         val minHashes1 = MinHasher.hash(shingles1, 100)
         val minHashes2 = MinHasher.hash(shingles2, 100)
 
-        val similarity = SimilarityCalculator.jaccard(minHashes1, minHashes2)
-        assertThat(similarity).isGreaterThan(0.5)
-        println(similarity)
+        val singlesSimilarity = SimilarityCalculator.jaccard(shingles1, shingles2)
+        val minHashSimilarity = SimilarityCalculator.jaccard(minHashes1, minHashes2)
+        val delta = abs(singlesSimilarity - minHashSimilarity)
+        assertThat(minHashSimilarity).isGreaterThan(0.5)
+        assertThat(delta).isLessThan(0.1)
+
+        println("Shingles similarity (slow): $singlesSimilarity")
+        println("MinHash similarity (fast): $minHashSimilarity")
+        println("Delta: $delta")
     }
 
     private fun readTokensFromFile(file: File): List<String> {
